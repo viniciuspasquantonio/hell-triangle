26dcc6131ddd98fea7b07d042ee5272492b11461
Vinicius Pasquantonio <vinicius.pasquantonio@talentfour.com.br>
2017-01-26 21:48:22 -0200

    commit inicial

---
+++ hell.triangle/src/main/java/br/com/pasquantonio/vinicius/hell/triangle/App.java
@@ -0,0 +1,13 @@
+package br.com.pasquantonio.vinicius.hell.triangle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
